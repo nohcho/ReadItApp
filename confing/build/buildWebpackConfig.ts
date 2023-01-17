97e16e961a129b83e93749a0af04646297eeaec9
Muhammad <tasuhanow@mail.ru>
2023-01-17 18:04:34 +0300

    absolute paths and new folders are added

--- confing/build/buildWebpackConfig.ts
+++ confing/build/buildWebpackConfig.ts
@@ -20,7 +20,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
             module: {
                 rules: buildLoaders(options)
               },
-              resolve: buildResolves(),
+              resolve: buildResolves(options),
               devtool: isDev ? 'inline-source-map' : undefined,
               // для того, чтобы в production режиме не было лишней информации, нагрузки
               devServer: isDev ? buildDevServer(options) : undefined
---
+++ src/app/providers/ThemeProvider/index.tsx
@@ -0,0 +1,3 @@
+import ThemeProvider from "app/providers/ThemeProvider/ui/Theme.Provider";
+import { useTheme } from "./lib/useTheme";
+export { ThemeProvider, useTheme }
