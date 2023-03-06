4e5e002e9541a1c5e26c283e811f21d2cfd33b42
Muhammad <tasuhanovw@mail.ru>
2023-03-06 21:53:45 +0300

    add redux store, add first entity

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,10 +1,13 @@
 import { useTranslation } from 'react-i18next';
+import { Counter } from 'entities/Counter';
 
 const MainPage = () => {
     const { t, i18n } = useTranslation('');
     return (
         <div>
             {t('Главная страница')}
+            <Counter />
+            <p>{t('Временный счетчик для проверки')}</p>
         </div>
     );
 };
