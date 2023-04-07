82960d21812662a687a3398464216f69caf478bb
Muhammad <tasuhanovw@mail.ru>
2023-04-07 19:17:28 +0300

    fix tests

--- src/features/AuthByUsername/model/slice/loginSlice.test.ts
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -10,13 +10,13 @@ describe('loginSlice.test', () => {
         expect(loginReducer(
 state as LoginSchema,
 loginActions.setUsername('Muhammad'),
-        )).toBe({ username: 'Muhammad' });
+        )).toEqual({ username: 'Muhammad' });
     });
     test('test set password', () => {
         const state: DeepPartial<LoginSchema> = { password: '213' };
         expect(loginReducer(
             state as LoginSchema,
             loginActions.setPassword('123'),
-        )).toBe({ password: '123' });
+        )).toEqual({ password: '123' });
     });
 });
