0c4b7eb5f5deb5703cb236b7b20d9deb3c90ec25
Muhammad <tasuhanovw@mail.ru>
2023-06-18 19:14:15 +0300

    add some guide to main page

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,13 +1,18 @@
 import { useTranslation } from 'react-i18next';
 import { Counter } from 'entities/Counter';
+import { Text, TextTheme } from 'shared/ui/Text/Text';
 
 const MainPage = () => {
-    const { t, i18n } = useTranslation('');
+    const { t } = useTranslation('');
     return (
         <div>
             {t('Главная страница')}
             <Counter />
             <p>{t('Временный счетчик для проверки')}</p>
+            <Text
+                theme={TextTheme.ERROR}
+                title={t('Для того, чтобы войти запустите бэкенд и введите логин и пароль, указанный в README')}
+            />
         </div>
     );
 };
